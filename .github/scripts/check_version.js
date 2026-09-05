21983548676468f8a5d81ee6f6adc2b3525219c5
hd <hdshare@vip.qq.com>
2026-09-05 11:35:54 +0800

    杂项: 优化工作流新增发送通知

--- .github/scripts/check_version.js
+++ .github/scripts/check_version.js
@@ -6,7 +6,10 @@ const { readFile, writeFile } = fs.promises;
 const repoRoot = process.cwd();
 const dyDataPath = path.join(repoRoot, "data", "dyData.json");
 
-async function getDownloadUrl(params) {
+const BOT_TOKEN = process.env.TG_BOT_TOKEN;
+const CHANNEL_ID = process.env.TG_CHANNEL_ID;
+
+async function getDownloadUrl() {
   const resp = await fetch(
     "https://api5-normal-lq.amemv.com/check_version/v7/?device_id=0&aid=1128&device_platform=android&os_api=36&update_version_code=40000000",
     {
@@ -19,6 +22,25 @@ async function getDownloadUrl(params) {
   const respJson = await resp.json();
   const downloadUrl = respJson.data.download_url;
   return downloadUrl;
+}
+
+async function sendNotify(text) {
+  const response = await fetch(
+    `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ chat_id: CHANNEL_ID, text }),
+    },
+  );
+  const result = await response.json();
+
+  if (!response.ok || !result.ok) {
+    console.log("发送通知失败");
+    return;
+  }
+
+  console.log(`发送通知成功`);
 }
 
 async function main() {
@@ -36,6 +58,9 @@ async function main() {
   dyData.push({ url: downloadUrl });
   await writeFile(dyDataPath, `${JSON.stringify(dyData, null, 2)}\n`, "utf8");
   console.log(`写入本地, 共 ${dyData.length} 条`);
+
+  const message = `检测到 DY 更新\n下载地址: ${downloadUrl}`;
+  await sendNotify(message);
 }
 
 main().catch((error) => {
